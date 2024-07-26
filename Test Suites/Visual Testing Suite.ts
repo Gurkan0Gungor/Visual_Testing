2af76b637b1edd16c188cd340dfd65dc14f648e2
Nuri Gürkan Güngör <gurkan.gungor@virgosol.com>
2024-07-26 11:23:11 +0300

    Visiual Test Suite Added

---
+++ Test Suites/Visual Testing Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Visual Testing Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>a7cd0edf-7f96-4284-9b99-8f44d2c8220f</testSuiteGuid>
+   <testCaseLink>
+      <guid>0f5d4ade-b046-40e1-a3bf-8eae7c4b6a04</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/visual Testing</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
